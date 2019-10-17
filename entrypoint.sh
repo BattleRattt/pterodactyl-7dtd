82e75e8ba680ce0d19cf5301b32fa7687d4987d9
BattleRatttt <39086978+BattleRattt@users.noreply.github.com>
2019-10-17 15:19:27 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
+sleep 2
+
 cd /home/container
-sleep 1
+
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
-    if [ ! -z ${SRCDS_BETAID} ]; then
-        if [ ! -z ${SRCDS_BETAPASS} ]; then
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
-        else
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
-        fi
-    else
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
-    fi
+    ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
 fi
 
 # Replace Startup Variables
@@ -22,4 +16,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
